13a51da58f8960cabed552d62ecd557b11607a11
Ludovic Tresson <tresson@intechinfo.fr>
2012-04-18 23:53:33 +0200

    Ajout du nom de la colonne, meilleur indentation
    Signed-off-by: Ludovic Tresson <tresson@intechinfo.fr>

--- application/views/membres.php
+++ application/views/membres.php
@@ -1,14 +1,21 @@
 	<!--Start of home page-->
 	<table border='1'>
-		<th> Pseudo </th><th> Date d'inscription </th>
+		<th> Pseudo </th><th> Date d'inscription </th><th> Ajouter un ami </th>
 	<?php
 	foreach($membres as $key)
 	{ ?>
 		<tr>
-			<td><?php echo $key['Pseudo'] ?></td>
-			<td><?php echo $key['RegisterDate'] ?></td>
-			<td><a href='index.php?page=membres.php&addFriend=<?php echo $key['IdUser'] ?>'>
-			<img src='<?php echo DIR_PUBLICS; ?>/images/plusIcon.jpg'></a></td>
+			<td>
+				<?php echo $key['Pseudo'] ?>
+			</td>
+			<td>
+				<?php echo $key['RegisterDate'] ?>
+			</td>
+			<td>
+				<a href='index.php?page=membres.php&addFriend=<?php echo $key['IdUser'] ?>'>
+					<img src='<?php echo DIR_PUBLICS; ?>/images/plusIcon.jpg'>
+				</a>
+			</td>
 		</tr>
 		<?php
 	}
