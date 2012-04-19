d00c4fc9cb6268e571c82f3e8926769c3855a8a7
Alexandre ARNAL <aarnal@intechinfo.fr>
2012-04-19 10:43:51 +0200

    Signed-off-by: Alexandre ARNAL <aarnal@intechinfo.fr>

--- application/modelsTest/register.php
+++ application/modelsTest/register.php
@@ -8,8 +8,12 @@ include("../models/mainModels.php");
 define('DS', '/');
 define('ADDRESS', '/');
 include("../../config/config.php");
-mysql_query("INSERT INTO Users (RegisterDate, Pseudo, Password, Mail) VALUES ('19/04/2012', 'Froutch', '123456', 'froutch@froutch.com)", dbConnect());
 
-register("19/04/2012", "Froutch", "123456", "froutch@froutch.com");
-echo $_SESSION["User"];
+$erreur = register("Froutch", "123456", "123456", "froutch@froutch.com", "Alexandre", "ARNAL", "19", "04", "2012", "5 impasse du moulin à vent", "Versailles", "France", "10609922424");
+$resultat = mysql_query ("SELECT * FROM Users WHERE Pseudo='Froutch'");
+$nbRow[] = mysql_fetch_assoc($resultat);
+echo "<p>";
+print_r($erreur);
+print_r($nbRow);
+echo "</p>";
 ?>
