40160972c37bf05d51b87a0eac1a97731e3f2a14
Alexandre ARNAL <aarnal@intechinfo.fr>
2012-04-19 09:18:30 +0200

    Signed-off-by: Alexandre ARNAL <aarnal@intechinfo.fr>

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -1,5 +1,26 @@
 	<!--Start of home page-->
 
-	<?php echo "coucou"; ?>
+<div id="homePage" tabindex="99998">
+	<form id="homeSearch" class="" style="width: 500px; margin-left: -250px;">
+	<div id="searchBar">
+	<div class="searchBar_left"></div>
+	<div class="searchBar_inner"></div>
+	<div class="searchBar_right"></div>
+	<div class="searchBar_inner_left"></div>
+	<div class="searchBar_inner_inner"></div>
+	<div class="searchBar_inner_right"></div>
+	</div>
+	<div id="searchBar_input">
+<input name="Mot" type="text" class="formulaireBoite" id="srchval" placeholder="Taper votre recherche ..." size="18">
+	<span id="searchBar_precomplete" class="hint" style="display: inline;"></span>
+	</div>
+	<button id="searchButton" name="Submit" type="button"></button>
+	</form>
+	
+ <script langage="text/javascript">
+function clearf(){
+document.getElementById('srchval').value="";
+}
+</script>
 	<!--End of home page-->
 	
