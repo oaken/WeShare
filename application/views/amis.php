73ece20832a938e61b09db97deb3934e7df3d220
Vincent <ricard@intechinfo.fr>
2012-04-19 10:19:11 +0200

    Signed-off-by: Vincent <ricard@intechinfo.fr>

---
+++ application/views/amis.php
@@ -0,0 +1,6 @@
+	<h3>Mes amis</h3>	
+	<p>
+		Vous êtes actuellement amis avec les personnes suivantes :
+	</p>
+	Vincent Ricard <a href="#"><em>cliquez ici pour supprimer cet ami</em></a><br />
+	Alexandre Arnal <a href="#"><em>cliquez ici pour supprimer cet ami</em></a><br />
---
+++ application/views/mesFilms.php
@@ -0,0 +1,11 @@
+<h3>Votre vidéothèque</h3>
+	<p>
+		#<em>Prénom Nom</em>, voici les films que vous avez ajouté à votre vidéothèque à ce jour :<br /></p>
+	<ul>
+		<!-- contiendra la/les requête/s SQL pour accéder aux films de l'utilisateur et les données liées tel que les formats et la disponibilité -->
+		<li> Titanic au/x format/s <br />#<em>format/s</em> et ils vous en reste #<em>nbr restant</em></li>
+		<li> Le Seigneur des Anneaux et la Communauté de l'Anneau <br />au/x format/s #<em>format/s</em> et ils vous en reste #<em>nbr restant</em></li>
+		<li> Le Seigneur des Anneaux et les Deux Tours <br />au/x format/s #<em>format/s</em> et ils vous en reste #<em>nbr restant</em></li>
+		<li> Le Seigneur des Anneaux et le Retour du Roi <br />au/x format/s #<em>format/s</em> et ils vous en reste #<em>nbr restant</em></li>
+		<li> Ironclad <br />au/x format/s #<em>format/s</em> et ils vous en reste #<em>nbr restant</em></li>
+	</ul>
