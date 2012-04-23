42b08d6e857c9d6a75057ecb4241489263da89d2
Vincent <ricard@intechinfo.fr>
2012-04-23 10:41:51 +0200

    Signed-off-by: Vincent <ricard@intechinfo.fr>

--- application/models/changeProfil.php
+++ application/models/changeProfil.php
@@ -6,21 +6,52 @@ define('DS', '/');
 define('ADDRESS', '/');
 include("../../config/config.php");
 
-//$link = dbConnect();
-//$query = sprintf('SELECT * FROM USERS WHERE Pseudo = %s', 'Mackovich');
-$result = mysql_query('SELECT * FROM USERS WHERE Pseudo = Mackovich', dbConnect());
-var_dump($result);
+$query_0 = sprintf(
+$query = sprintf('SELECT * FROM USERS WHERE IdUSer = %d', getId("Mackovich"));
+$result = mysql_query($query, dbConnect());
+$row = mysql_fetch_row($result);
 if (isset($result) )
 {
-	mysq
+	$it = 0;
+	while (isset($row[$it]))
+	{
+		echo($row[$it++].'<br />');
+	}
 }
 else
 {
 	echo('fail');
 }
+/*
+RegisterDate		DATE			NOT NULL,
+Pseudo			 	VARCHAR(255) 	NOT NULL,
+Password			VARCHAR(255) 	NOT NULL,
+Mail			 	VARCHAR(255) 	NOT NULL,
+LastName			VARCHAR(255),
+FirstName			VARCHAR(255),
+BornDate			DATE,
+Address			 	VARCHAR(255),
+City			 	VARCHAR(255),
+Country			 	VARCHAR(255),
+Phone				DECIMAL(12,0),
+Avatar
+*/
+// ------------------------------------------------------- //
+function	changeProfil($Pseudo, $FirstName, $LastName, $Password, $Mail, 
+						$BornDate, $Adress, $City, $Country, $Phone, $Avatar)
+{
+	if (!empty($FirstName))
+	{
+		$query = sprintf('UPDATE USERS SET FirstName = %s 
+						  WHERE Pseudo = %s 
+						  AND IdUSer = %d', 
+						  $FirstName, $Pseudo, $getId($Pseudo));
+		mysql_query();
+	}
+	else if (!empty())
+	{
+	
+	}
+}
 
-switch 
-
-$psedo.$password
-Pseudo, 
 ?>
